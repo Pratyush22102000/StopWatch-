fd54e019b30fb822ff8b7f05f3760a4ded179260
Pratyush Upadhyaya <pratyushsarni23@gmail.com>
2022-09-20 23:34:30 +0530

    StopWatch using chronometer

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chronometer= findViewById(R.id.chronometer);
+
+    }public void startChronometer(View view)
+    {if(!running)
+    {   chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+        chronometer.start();
+        running=true;
+    }
+
+    }
+    public void pauseChronometer(View view)
+    {if(running)
+    {
+        chronometer.stop();
+        pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+        running=false;
+    }
+
+    }
+    public void resetChronometer(View view)
+    {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset=0;
+    }
+}
